0e4a1819934e40a5aaeee199b504ec46bc4ecf43
André Ferreira <contato@andreferreira.me>
2018-09-30 20:43:44 -0300

    Add Google Tag Manager Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-N3Q4DJ",
+        includeInDevelopment: false,
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+      },
+    },
   ],
 }

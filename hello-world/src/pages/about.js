a3983492f33c74a884b586f1e655c8fb2edb988b
André Ferreira <contato@andreferreira.me>
2018-09-30 17:35:48 -0300

    gatsby tutorials

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div>
+    <Header headerText="Sobre mim" arbitraryPhrase="is arbitrary" />
+    <Header headerText="It's pretty cool" />
+    <p>André Ferreira, 28 anos, morando atualmente em Curitiba / PR. Formado em Comunicação Social – Publicidade, Propaganda e Marketing, atuei por 4 anos em agências de comunicação no planejamento e execução de campanhas de mídias sociais, links patrocinados e SEO para empresas nacionais e multinacionais. Também possuo certificação em Google Analytics pelo Google Partners desde 2014.</p>
+    <p>Neste período cresceu o meu interesse por tecnologias de desenvolvimento, o que me levou a realizar o curso de Tecnologia em Sistemas para Internet para aprimorar os meus conhecimentos nesta área.</p>
+    <p>Nos tempos atuais, a publicidade e o marketing andam lado a lado com a tecnologia e dominar ambas as áreas me permitem desenvolver as soluções adequadas para atingir os objetivos de cada negócio.</p>
+    <p>Atualmente, trabalho como desenvolvedor web na NegocieCoins, uma plataforma de negociação de Bitcoin, que está transformando o modo como lidamos com dinheiro no mundo.</p>
+    <p>Entre em contato através do e-mail <a href="mailto:contato@andreferreira.me">contato@andreferreira.me</a>.</p>
+  </div>
+)

e5da2d4749935b3176df89d46ddcf570a68d9161
André Ferreira <contato@andreferreira.me>
2018-09-30 17:39:18 -0300

    Delete gatsby tutorials

--- tutorial-part-two/src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-
-// import bootstrapTheme from "typography-theme-bootstrap"
-import fairyGatesTheme from "typography-theme-fairy-gates"
-
-const typography = new Typography(fairyGatesTheme)
-
-export default typography

cbb643aafefe6f93f8b731153597e586f1a757da
André Ferreira <contato@andreferreira.me>
2018-10-01 00:49:15 -0300

    Add Home content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import Menu from '../components/Menu'
+import HomeLayout from "../layouts/HomeLayout"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default () => (
-  <div>
+  <HomeLayout>
     <h1>André Ferreira</h1>
     <h2>Full Stack Developer</h2>
-    <h3>Programming, Blockchain and more.</h3>
-    <Menu />
-  </div>
+    <h4>Programming, Blockchain and more.</h4>
+    <FontAwesomeIcon icon={faGithub} />
+    <FontAwesomeIcon icon={faTwitter} />
+    <FontAwesomeIcon icon={faFacebook} />
+    <FontAwesomeIcon icon={faInstagram} />
+    <FontAwesomeIcon icon={faLinkedin} />
+  </HomeLayout>
 )

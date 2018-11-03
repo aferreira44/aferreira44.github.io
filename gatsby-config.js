d8a3913fcb054d5e068910b405a450ec72909d89
André Ferreira <contato@andreferreira.me>
2018-11-03 16:46:10 -0300

    Add plugins to use markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://andreferreira.me`,
+    siteUrl: 'https://andreferreira.me',
+    title: 'André Ferreira',
+    subtitle: 'Full Stack Developer',
+    description: 'Programming, Blockchain, AI and Machine Learning, Open Source, GNU/Linux, Privacy and Freedom'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/`,
+        name: 'src',
+      },
+    },
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
@@ -12,25 +24,22 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-nprogress`,
+      resolve: 'gatsby-plugin-nprogress',
       options: {
-        color: `tomato`,
+        color: 'tomato',
         showSpinner: true,
       },
     },
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: 'src/utils/typography',
       },
     },
     {
-      resolve: `gatsby-plugin-emotion`,
-    },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
+      resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: "GTM-N3Q4DJ",
+        id: 'GTM-N3Q4DJ',
         includeInDevelopment: false,
       },
     },

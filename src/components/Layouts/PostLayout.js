d60cb502127a6ab0954f9f7892bc953e0411dc1f
André Ferreira <contato@andreferreira.me>
2018-11-08 10:05:50 -0200

    Remove hr and title class

--- src/components/Layouts/PostLayout.js
+++ src/components/Layouts/PostLayout.js
@@ -6,9 +6,10 @@ export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
-      <h1 className="title">{post.frontmatter.title}</h1>
+      <h1>{post.frontmatter.title}</h1>
       <div>{post.frontmatter.slug}</div>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <hr />
     </Layout>
   )
 }

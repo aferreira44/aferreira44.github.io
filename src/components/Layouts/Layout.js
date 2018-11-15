4e66e3b10a29c3703df9d112c7405d76e27f8ac6
André Ferreira <contato@andreferreira.me>
2018-11-15 09:08:18 -0200

    Fix GTM script loading (#22)

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -5,14 +5,28 @@ import Footer from '../UI/Footer'
 import Header from '../UI/Header'
 
 class Layout extends React.Component {
+
   componentDidMount() {
     /* Google Tag Manager */
-    if (window.location.hostname !== 'localhost') {
+    if (!['localhost', 's3', 'cloudfront'].includes(window.location.hostname)) { // TODO: Use env variables for this
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!=='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-N3Q4DJ');
+
+      const noscript = document.createElement('noscript');
+      const iframe = document.createElement('iframe');
+
+      noscript.appendChild(iframe);
+
+      iframe.setAttribute("title", "GoogleTagManager");
+      iframe.setAttribute("src", "https://www.googletagmanager.com/ns.html?id=GTM-N3Q4DJ");
+      iframe.setAttribute("height", "0");
+      iframe.setAttribute("width", "0");
+      iframe.setAttribute("style", "display:none;visibility:hidden");
+
+      document.body.prepend(noscript);
     }
     /* End Google Tag Manager */
   }
@@ -30,10 +44,6 @@ class Layout extends React.Component {
         <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Helmet>
-      {/* Google Tag Manager (noscript) */}
-      <noscript><iframe title="GoogleTagManager" src="https://www.googletagmanager.com/ns.html?id=GTM-N3Q4DJ"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      {/* End Google Tag Manager (noscript) */}
       <Menu />
       <Header />
       { children }

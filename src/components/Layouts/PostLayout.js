3542908f351bdc782e18f4ff8958a0c057230156
André Ferreira <andre.ferreira@fohat.co>
2020-04-04 14:25:48 -0300

    Use Ant Design instead of Material UI

--- src/components/Layouts/PostLayout.js
+++ src/components/Layouts/PostLayout.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import { graphql } from "gatsby"
-import Layout from "../Layouts/Layout"
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../Layouts/Layout";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <div css={`
-      & .timeToRead {
-        font-weight: bold;
-      }
-    `}>
+    <div
+      css={`
+        & .timeToRead {
+          font-weight: bold;
+        }
+      `}
+    >
       <Layout>
         <h1>{post.frontmatter.title}</h1>
-        <p><span>Published on {post.frontmatter.date}</span> - <span className="timeToRead">Time to read: {post.timeToRead} min.</span></p>
+        <p>
+          <span>Published on {post.frontmatter.date}</span> -{" "}
+          <span className="timeToRead">
+            Time to read: {post.timeToRead} min.
+          </span>
+        </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr />
       </Layout>
     </div>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query($slug: String!) {
@@ -31,4 +37,4 @@ export const query = graphql`
       timeToRead
     }
   }
-`
+`;
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { graphql, StaticQuery } from 'gatsby'
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
 
 export default () => (
   <StaticQuery
@@ -14,24 +13,26 @@ export default () => (
         }
       }
     `}
-    render={data => (
-      <div css={`
-        & .title, .description {
-          text-align: center;
-        }
+    render={(data) => (
+      <div
+        css={`
+          & .title,
+          .description {
+            text-align: center;
+          }
 
-        & .title {
-          margin: 0 0 30px 0;
-        }
-      `}>
+          & .title {
+            margin: 0 0 30px 0;
+          }
+        `}
+      >
         <Typography variant="h3" component="h1" align="center">
           {data.site.siteMetadata.title}
         </Typography>
         <Typography variant="subtitle1" align="center">
           {data.site.siteMetadata.description}
         </Typography>
-        <hr />
       </div>
     )}
   />
-)
+);

5dfc065be64921f62b6571a7236ee2565735d2c4
André Ferreira <contato@andreferreira.me>
2020-04-20 02:57:10 -0300

    Fix css string prop

--- src/components/Layouts/PostLayout.js
+++ src/components/Layouts/PostLayout.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Row, Col } from "antd";
 import Layout from "../Layouts/Layout";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <div
-      css={`
-        & .timeToRead {
-          font-weight: bold;
-        }
-      `}
-    >
+    <div>
       <Layout>
-        <h1>{post.frontmatter.title}</h1>
-        <p>
-          <span>Published on {post.frontmatter.date}</span> -{" "}
-          <span className="timeToRead">
-            Time to read: {post.timeToRead} min.
-          </span>
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Row justify="center">
+          <Col span={12}>
+            <h1>{post.frontmatter.title}</h1>
+            <p>
+              <span>Published on {post.frontmatter.date}</span> -{" "}
+              <span
+                css={{
+                  fontWeight: "bold",
+                }}
+              >
+                Time to read: {post.timeToRead} min.
+              </span>
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </Col>
+        </Row>
       </Layout>
     </div>
   );

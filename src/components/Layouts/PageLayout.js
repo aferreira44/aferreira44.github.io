4325c3c813494d69acd13939b16abc3ffffd3e6b
André Ferreira <contato@andreferreira.me>
2020-04-20 02:36:43 -0300

    Ajust title and paragraphs width

--- src/components/Layouts/PageLayout.js
+++ src/components/Layouts/PageLayout.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import Layout from "../Layouts/Layout"
+import React from "react";
+import Layout from "../Layouts/Layout";
+import { Row, Col } from "antd";
 
 export default ({ children }) => (
-  <Layout>{children}</Layout>
-)
+  <Layout>
+    <Row justify="center">
+      <Col span={12}>{children}</Col>
+    </Row>
+  </Layout>
+);

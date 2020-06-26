53e0639748b9fcceb493731b9d682d616c5b52b5
André Ferreira <contato@andreferreira.me>
2020-06-26 16:34:44 -0300

    Improve posts font

--- src/components/Layouts/PostLayout.js
+++ src/components/Layouts/PostLayout.js
@@ -7,6 +7,7 @@ import { MDXProvider } from "@mdx-js/react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { Link } from "gatsby";
 import Layout from "../Layouts/Layout";
+import { Helmet } from "react-helmet";
 
 const { Title, Text } = Typography;
 const shortcodes = { Link }; // Provide common components here
@@ -19,9 +20,18 @@ export default ({ data }) => {
         styles={css`
           p {
             font-size: 21px;
+            font-family: "Roboto Slab", serif;
+            font-weight: 100;
+            color: #292929;
           }
         `}
       />
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:400&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Row justify="center">
         <Col span={12}>
           <Title>{post.frontmatter.title}</Title>
